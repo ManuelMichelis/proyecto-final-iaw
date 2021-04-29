ab50874b010b13b563d1c066f707135655c0c7f0
manum2694 <manuelmichelis94@gmail.com>
2021-04-29 10:31:52 -0300

    Actualización 0: Setup inicial + agregación de credenciales para la BD

---
+++ app/Models/Usuario.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Usuario extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * Atributos agregados manualmente
+     *
+     */
+    public $incrementing = false;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'email';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'alias',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
