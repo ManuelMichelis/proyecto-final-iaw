5dd57c7882ee2b7f9590c7b730aded3c64072e72
ManuelMichelis <manuelmichelis94@gmail.com>
2021-05-08 19:40:44 -0300

    Actualización 2 - Entidades y relaciones listas + listados de posteos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,21 @@
             {{ __('Inicio') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <br>
+    @foreach ($posteos as $posteo)
+        <div class="py-4">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-5 bg-white border-b border-gray-200">
+                        {{ $posteo->alias_usuario }} - {{ $posteo->topicos()->first()->nombre }}
+                        <div class="bg-white overflow-hidden sm:rounded-lg">
+                            <div class="p-2 bg-white"></div>
+                                {{ $posteo->contenido }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endforeach
 </x-app-layout>
