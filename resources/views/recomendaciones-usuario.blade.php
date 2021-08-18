71ab9eca8e80fe3119c1041983a3e8a999e3a816
ManuelMichelis <manuelmichelis94@gmail.com>
2021-08-18 19:47:28 -0300

    Refactory de vista de recomendaciones de usuarios

--- resources/views/recomendaciones-usuario.blade.php
+++ resources/views/recomendaciones-usuario.blade.php
@@ -23,61 +23,71 @@
         @if (count($usuarios) > 0)
             <div class="container">
                 <div class="m-4">
-                    <div class="card-deck">
-                        @foreach ($usuarios as $usuario)
-                            <div class="col-sm-6 col-md-4 col-lg-3">
-                                <div class="card mb-4">
-                                    <div class="m-2">
-                                        <div class="d-flex justify-content-center">
+                    @foreach ($usuarios as $usuario)
+                        <div class="card mb-4">
+                            <div class="m-2">
+                                <div class="d-flex justify-content-between">
+                                    <div class="p-2">
+                                        <div class="d-flex justify-content-start">
                                             <div class="d-flex">
                                                 <span class="material-icons" style="font-size: 100px">
                                                     account_circle
                                                 </span>
                                             </div>
+                                            <div class="flex-column align-self-center">
+                                                <div class="m-3">
+                                                    <div class="d-flex">
+                                                        <h5 class="card-title">
+                                                            <b>
+                                                                {{ $usuario->alias }}
+                                                            </b>
+                                                        </h5>
+                                                    </div>
+                                                    <div class="d-flex">
+                                                        <p id="contenedor_seguidores_{{ $usuario->id }}" class="card-text">
+                                                            <b>
+                                                                {{ count($usuario->seguidores) }}
+                                                            </b>
+                                                            seguidores
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="card-body text-center">
-                                        <h5 class="card-title">
-                                            <b>
-                                                {{ $usuario->alias }}
-                                            </b>
-                                        </h5>
-                                        <p id="contenedor_seguidores_{{ $usuario->id }}" class="card-text">
-                                            <b>
-                                                {{ count($usuario->seguidores) }}
-                                            </b>
-                                            seguidores
-                                        </p>
-                                        <div class="d-flex justify-content-center">
-                                            @if (Auth::user()->sigue($usuario))
-                                                <button
-                                                    id="btn_seguimiento"
-                                                    class="btn estado-siguiendo"
-                                                    onclick="requestActualizarSeguimiento(
-                                                            {{ $usuario->id }},
-                                                            '{{ csrf_token() }}',
-                                                            '{{ route('actualizarSeguimiento',['id'=> $usuario->id]) }}'
-                                                    )">
-                                                    Dejar de seguir
-                                                </button>
-                                            @else
-                                                <button
-                                                    id="btn_seguimiento"
-                                                    class="btn estado-sin-seguir"
-                                                    onclick="requestActualizarSeguimiento(
-                                                            {{ $usuario->id }},
-                                                            '{{ csrf_token() }}',
-                                                            '{{ route('actualizarSeguimiento',['id'=> $usuario->id]) }}'
-                                                    )">
-                                                    Seguir
-                                                </button>
-                                            @endif
+                                    <div class="d-flex align-self-center">
+                                        <div class="m-3">
+                                            <div class="d-flex justify-content-center">
+                                                @if (Auth::user()->sigue($usuario))
+                                                    <button
+                                                        id="btn_seguimiento"
+                                                        class="btn estado-siguiendo"
+                                                        onclick="requestActualizarSeguimiento(
+                                                                {{ $usuario->id }},
+                                                                '{{ csrf_token() }}',
+                                                                '{{ route('actualizarSeguimiento',['id'=> $usuario->id]) }}'
+                                                        )">
+                                                        Dejar de seguir
+                                                    </button>
+                                                @else
+                                                    <button
+                                                        id="btn_seguimiento"
+                                                        class="btn estado-sin-seguir"
+                                                        onclick="requestActualizarSeguimiento(
+                                                                {{ $usuario->id }},
+                                                                '{{ csrf_token() }}',
+                                                                '{{ route('actualizarSeguimiento',['id'=> $usuario->id]) }}'
+                                                        )">
+                                                        Seguir
+                                                    </button>
+                                                @endif
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         @else
